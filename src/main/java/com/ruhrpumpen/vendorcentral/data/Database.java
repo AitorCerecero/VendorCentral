a992b1efa545691ed29c390abc89d56ba06d52a0
Ricardo DB <ricardo.domber96@gmail.com>
2025-03-26 18:50:55 -0600

    Add simple DB connection as sample

---
+++ src/main/java/com/ruhrpumpen/vendorcentral/data/Database.java
@@ -0,0 +1,15 @@
+package com.ruhrpumpen.vendorcentral.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String URL = "jdbc:mysql://localhost:3306/miapp_db"; // URL de la DB a usar
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.ruhrpumpen.vendorcentral {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.ruhrpumpen.vendorcentral to javafx.fxml;
     exports com.ruhrpumpen.vendorcentral;
